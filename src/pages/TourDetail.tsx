609be4e0b0e52bd4a6b9511036a03a568730f404
hemidirasim <hamidirasim@gmail.com>
2025-10-06 16:31:46 +0400

    Add debug info for max_participants field
    - Add debug information to show max_participants and group_size values
    - Help identify why Max Participants is not displaying in Group Size
    - Debug info only shows in development mode
    - Will help troubleshoot the data flow from API to frontend

--- src/pages/TourDetail.tsx
+++ src/pages/TourDetail.tsx
@@ -278,6 +278,12 @@ const TourDetail = () => {
                           {tour.max_participants ? `Up to ${tour.max_participants} participants` : 
                            tour.group_size || 'Small group (4-8 people)'}
                         </div>
+                        {/* Debug info - remove in production */}
+                        {process.env.NODE_ENV === 'development' && (
+                          <div className="text-xs text-gray-500 mt-1">
+                            Debug: max_participants={tour.max_participants}, group_size={tour.group_size}
+                          </div>
+                        )}
                       </div>
                     </div>
                     <div className="flex items-center space-x-3">
