804ebd0c1a7966ccf9fb5c65f87ce951827e442c
hemidirasim <hamidirasim@gmail.com>
2025-10-27 16:45:31 +0400

    Remove height constraint from footer logo
    - Remove h-28 class from Logo component in Footer
    - Keep only w-28 for width constraint
    - Logo will now maintain its aspect ratio naturally
    - This prevents logo distortion in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -162,7 +162,7 @@ const Footer = () => {
           {/* Company Info - Right */}
           <div className="lg:w-1/4 space-y-4 text-center lg:text-right">
             <div className="flex items-center justify-center lg:justify-end">
-              <Logo className="w-28 h-28" />
+              <Logo className="w-28" />
             </div>
             <p className="text-gray-900 text-sm leading-relaxed">
               Founded in 2014 as Camping Azerbaijan, Outtour Azerbaijan is recognised as the country's first ecotour operator. We specialise in sustainable mountain adventures, cultural immersion and community-led experiences.
