24657d0ff092c38c7c34cd54af97164196970d62
hemidirasim <hamidirasim@gmail.com>
2025-10-29 17:26:23 +0400

    Simplify submit message - show as plain text below submit button instead of styled alert box

--- src/components/TailorMadeForm.tsx
+++ src/components/TailorMadeForm.tsx
@@ -424,29 +424,13 @@ const TailorMadeForm = () => {
             
             {/* Submit Message */}
             {submitMessage && (
-              <div className={`mt-4 p-4 rounded-lg text-center ${
+              <p className={`mt-4 text-center text-sm ${
                 submitMessage.type === 'success' 
-                  ? 'bg-green-50 text-green-700 border border-green-200' 
-                  : 'bg-red-50 text-red-700 border border-red-200'
+                  ? 'text-green-600' 
+                  : 'text-red-600'
               }`}>
-                <div className="flex items-center justify-center gap-2">
-                  {submitMessage.type === 'success' ? (
-                    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                      <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-                    </svg>
-                  ) : (
-                    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                      <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-                    </svg>
-                  )}
-                  <span className="font-medium">
-                    {submitMessage.type === 'success' ? 'Success!' : 'Error:'}
-                  </span>
-                </div>
-                <p className="mt-1 text-sm">
-                  {submitMessage.text}
-                </p>
-              </div>
+                {submitMessage.text}
+              </p>
             )}
           </div>
         </form>
