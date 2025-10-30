3a1358acc4309fe1e22e0983dda26496ee8fa53f
hemidirasim <hamidirasim@gmail.com>
2025-10-30 13:12:23 +0400

    Footer: use provided X (Twitter) SVG icon in social links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -91,8 +91,9 @@ const Footer = () => {
     add(t?.instagram, Instagram, 'Instagram');
     add(t?.linkedin, Linkedin, 'LinkedIn');
     add(t?.twitter, (props: any) => (
-      <svg width="20" height="20" viewBox="0 0 24 24" {...props}>
-        <path d="M18 6L6 18M6 6l12 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 509.64" {...props}>
+        <rect width="512" height="509.64" rx="115.61" ry="115.61"/>
+        <path fill="#fff" fillRule="nonzero" d="M323.74 148.35h36.12l-78.91 90.2 92.83 122.73h-72.69l-56.93-74.43-65.15 74.43h-36.14l84.4-96.47-89.05-116.46h74.53l51.46 68.04 59.53-68.04zm-12.68 191.31h20.02l-129.2-170.82H180.4l130.66 170.82z"/>
       </svg>
     ), 'X');
     add(t?.youtube, Youtube, 'YouTube');
